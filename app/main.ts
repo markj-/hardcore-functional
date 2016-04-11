7eef17f750012d84143a969639038b90059374bd
Mark Jones <mark@getkahoot.com>
2016-04-11 20:09:00 +0100

    Add type signatures to main.ts

--- app/main.ts
+++ app/main.ts
@@ -12,10 +12,13 @@ import log from 'utils/log';
 
 const apiEndpoint: string = 'http://jsonplaceholder.typicode.com/posts';
 
+// templatePost :: String -> String
 const templatePost = (title: string) => `<li>${title}</li>`;
 
+// getPostHtml :: Post -> String
 const getPostHtml = compose(templatePost, _.prop('title'));
 
+// getPostsHtml :: [Post] -> String
 const getPostsHtml = compose(_.join(''), map(getPostHtml));
 
 const displayPosts = compose(setHtml('.posts'), getPostsHtml);
